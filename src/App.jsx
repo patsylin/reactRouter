2eacac0730b3286112b5ac70325b847bbe995f35
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2023-08-20 16:53:08 -0500

    finished project!

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
 import Red from './Red'
 import Blue from './Blue'
+import Home from './components/Home'
 
 function App() {
   return (
     <div id="container">
       <div id="navbar">
-        <Link to="/blue">blue</Link>
-        <Link to="/red">red</Link>
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/home">Home</Link>
       </div>
       <div id="main-section">
         <Routes>
           <Route path="/blue" element={<Blue/>} />
           <Route path="/red" element={<Red/>} />
+          <Route path="/home" element={<Home/>} />
         </Routes>
       </div>
     </div>
---
+++ src/components/Home.jsx
@@ -0,0 +1,7 @@
+export default function Blue() {
+    return (
+      <div className="home">
+        <h1>HOME</h1>
+      </div>
+    );
+  }
