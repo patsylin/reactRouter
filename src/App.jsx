4391665913882fd0f5d66770540d4c39b03d90f6
Patsy O'Brien <97324605+patsylin@users.noreply.github.com>
2023-08-20 16:40:54 -0500

    first commit. something is showing up on the page!

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Red from './Red'
+import Blue from './Blue'
+
+function App() {
+  return (
+    <div id="container">
+      <div id="navbar">
+        <Link to="/blue">blue</Link>
+        <Link to="/red">red</Link>
+      </div>
+      <div id="main-section">
+        <Routes>
+          <Route path="/blue" element={<Blue/>} />
+          <Route path="/red" element={<Red/>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
